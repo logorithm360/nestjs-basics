bae02d8a10c5439406100230f251395c3b95a6e2
logorithm360 <logorithm360@gmail.com>
2026-03-19 01:59:28 +0300

    Setting up a database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule,ConfigModule.forRoot({isGlobal: true}), DatabaseModule],
+  
 })
 export class AppModule {}
